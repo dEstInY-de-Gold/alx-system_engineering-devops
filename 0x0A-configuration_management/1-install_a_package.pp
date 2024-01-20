2c1cb6673b584d27fc3ba3ae9b4f6f1e475f020b
Noah <noahayebu@gmail.com>
2024-01-20 05:25:28 +0000

    Execute a command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
-# To creates a file
-file { '/tmp/school':
-	ensure  => file,
-	path    => '/tmp/school',
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love Puppet'
+# Installing puppet-lint
+package { 'puppet-lint':
+	ensure   => '2.5.0',
+	provider => 'gem'
 }
