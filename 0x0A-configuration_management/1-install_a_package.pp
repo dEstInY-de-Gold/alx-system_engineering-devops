4f91878d323ab67532727686d6cd18644cf62045
Noah <noahayebu@gmail.com>
2024-01-20 04:57:29 +0000

    Create file

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+# To creates a file
+file { '/tmp/school':
+	ensure  => file,
+	path    => '/tmp/school',
+	mode    => '0744',
+	owner   => 'www-data',
+	group   => 'www-data',
+	content => 'I love Puppet'
+}
