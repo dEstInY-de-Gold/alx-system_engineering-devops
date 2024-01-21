9c4d138aefd48f89a391a330ef0414edbdc6b13c
Noah <noahayebu@gmail.com>
2024-01-21 06:02:48 +0000

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
-file { home/noah/.ssh/config':
+file { home/noah/.ssh/config:
 	ensure  => file,
-	owner   => 'ubuntu',
-	group   => '52.90.22.126',
-	mode    => '0600',
-	content => '
-		Host 52.90.22.126
-		IdentityFile home/noah/.ssh/school
+	owner   => ubuntu,
+	group   => 54.161.235.10,
+	mode    => 0600,
+	content =>
+		Host 54.161.235.10
+		IdentityFile /home/noah/.ssh/school
 		PreferredAuthentications publickey
 		PasswordAuthentication no
-	',
+	,
 }
