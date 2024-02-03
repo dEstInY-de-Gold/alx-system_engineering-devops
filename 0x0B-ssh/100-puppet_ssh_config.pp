2aacb79fc33fe9e70dfcdbf172a4709c328dde08
Noah <noahayebu@gmail.com>
2024-02-03 03:26:01 +0000

    www

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,7 +5,7 @@ file_line { 'Turn off passwd auth':
 		line   => '    PasswordAuthentication no',
 }
 
-		file_line { 'Declare identity file':
+file_line { 'Declare identity file':
 		ensure => 'present',
 		path   => '/etc/ssh/ssh_config',
 		line   => '    IdentityFile ~/.ssh/school',
